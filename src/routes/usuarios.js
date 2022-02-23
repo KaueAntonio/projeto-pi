f06f1a74d091fb200fa0a538880087ef2ef4e288
KaueAntonio <kaue.santos@bandtec.com.br>
2022-02-23 13:20:54 -0300

    Subindo Template do Site

---
+++ src/routes/usuarios.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+
+var usuarioController = require("../controllers/usuarioController");
+
+router.get("/", function(req, res) {
+    usuarioController.testar(req, res);
+});
+
+router.get("/listar", function(req, res) {
+    usuarioController.listar(req, res);
+});
+
+module.exports = router;
