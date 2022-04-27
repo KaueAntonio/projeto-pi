bbd5fcf0674d78444e86518306ae6085dffba4f0
WallisonGalvao <wallison.quero@bandtec.com.br>
2022-04-27 19:35:40 -0300

    feat: integracao com slack (trocar webHookURL)

--- testeProjeto/src/main/java/com/sptech/testeprojeto/integracao/slack/IntegracaoSlack.java
+++ testeProjeto/src/main/java/com/sptech/testeprojeto/integracao/slack/IntegracaoSlack.java
@@ -12,6 +12,8 @@ import com.sptech.testeprojeto.Connection;
 import org.springframework.jdbc.core.JdbcTemplate;
 import com.sptech.testeprojeto.integracao.slack.SlackMapper;
 import com.sptech.testeprojeto.integracao.slack.SlackTest;
+import java.util.List;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 /**
  *
@@ -22,19 +24,21 @@ public class IntegracaoSlack {
     public static void main(String[] args) {
         Connection config = new Connection();
         JdbcTemplate template = new JdbcTemplate(config.getDataSource());
-        String queryAlerta = "SELECT TOP(1) descricao FROM "
+        String queryAlerta = "SELECT TOP(1) * FROM "
                 + "[dbo].[log_alertas] JOIN [dbo].[log_registros] "
                 + "ON fk_registro = id_registro ORDER BY id_log_alerta DESC";
+        
 
-        SlackTest teste = template.queryForObject(queryAlerta, new SlackMapper());
-        String descricao = String.format("%s", teste.getDescricao());
+        List<SlackTest> teste = template.query(queryAlerta, new BeanPropertyRowMapper<>(SlackTest.class));
+        System.out.println("String: " + teste.get(0).getDescricao());
+        String descricao = String.format("%s", teste.get(0).getDescricao());
         enviarMensagem(descricao);
 
     }
 
     public static void enviarMensagem(String message) {
         try {
-            String webHookURL = "https://hooks.slack.com/services/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";
+            String webHookURL = "https://hooks.slack.com/services/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";
             String slackChannel = "equipe-ti";
             StringBuilder msgBuilder = new StringBuilder();
 
