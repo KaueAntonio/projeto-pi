2fbbcfc35bd6873f6ca5cc5a007c78a819d20826
KaueAntonio <kaue.santos@bandtec.com.br>
2022-04-30 11:30:15 -0300

    Melhorias Site: Querys SQL e Ajuste de Informações na Dashboard

---
+++ public/js/dash.js
@@ -0,0 +1,13 @@
+var dadosMesesCliques1 = 0;
+var dadosMesesCliques2 = 0;
+var dadosMesesCliques3 = 0;
+var dadosMesesCliques4 = 0;
+var dadosMesesCliques5 = 0;
+var dadosMesesCliques6 = 0;
+var dadosMesesCliques7 = 0;
+var dadosMesesCliques8 = 0;
+var dadosMesesCliques9 = 0;
+var dadosMesesCliques10 = 0;
+var dadosMesesCliques11 = 0;
+var dadosMesesCliques12 = 0;
+
