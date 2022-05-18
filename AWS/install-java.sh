53272d6702880c6d14a0f60ecff742fc688694ca
KaueAntonio <kaue.santos@bandtec.com.br>
2022-05-17 21:18:08 -0300

    Shell

--- AWS/install-java.sh
+++ AWS/install-java.sh
@@ -1,8 +1,8 @@
 FROM ubuntu:latest
 
-RUN sudo add-apt-repository ppa:webupd8team/java -y
-RUN sudo apt update -y
-RUN sudo apt install default-jre ; apt install openjdk-11-jre-headless; -y
+RUN add-apt-repository ppa:webupd8team/java -y
+RUN apt update -y
+RUN apt install default-jre ; apt install openjdk-11-jre-headless; -y
 RUN git clone https://www.github.com/kaueantonio/projeto-pi.git
 RUN cd AWS
 RUN java -jar testeProjeto-SERVER.java
