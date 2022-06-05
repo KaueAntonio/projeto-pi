15c5cdc196d59882d1cb78ec1c4ed77e8b17cb3a
KaueAntonio <kaue.santos@bandtec.com.br>
2022-06-05 18:48:54 -0300

    Retoques Dashboard

--- public/js/src/moeda.js
+++
@@ -1,7 +0,0 @@
-export default (valor, parametro) => {
-  if (valor !== null && valor !== undefined) {
-    return valor.toLocaleString('pt-BR', { minimumFractionDigits: parametro })
-  } else {
-    return valor
-  }
-}
